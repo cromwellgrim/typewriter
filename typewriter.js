c5badab759dfd6f875e2dc79cdc13e2954781ad3
cromwellgrim <rogers.dylan@protonmail.com>
2021-02-09 23:18:10 +0000

    typewriter updated

--- typewriter.js
+++ typewriter.js
@@ -1,16 +1,16 @@
 const sentence = 'hello there from lighthouse labs';
-const typist = function () {
-for (let char of sentence) {
+const typist = function() {
+  for (let char of sentence) {
 
-  setTimeout( () => {
-    setTimeout( () => {
-      process.stdout.write(char);
-    }, 50);
-  }, 1000);
-}
-setTimeout( () => {
-  console.log(' ');
-}, 1100);
+    setTimeout(() => {
+      setTimeout(() => {
+        process.stdout.write(char);
+      }, 50);
+    }, 1000);
+  }
+  setTimeout(() => {
+    console.log(' ');
+  }, 1100);
 };
 
 // typist(sentence);
